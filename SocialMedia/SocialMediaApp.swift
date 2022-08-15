f6836a336bb2ed4a320245120e4a0be2792b2124
Andres <andres.gonzalez2392@gmail.com>
2022-08-15 14:50:40 -0400

    Initial Commit

---
+++ SocialMedia/SocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaApp.swift
+//  SocialMedia
+//
+//  Created by PDWS on 8/15/22.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
